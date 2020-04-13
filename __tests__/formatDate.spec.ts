02c6191d38fa3672bde7a390413dd89764ffe1fe
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-04-13 22:30:45 +0200

    Correct tests.

--- __tests__/formatDate.spec.ts
+++ __tests__/formatDate.spec.ts
@@ -9,27 +9,27 @@ describe('formatDate', () => {
     });
   });
 
-  it('returns date object for 2021-13-10', () => {
+  it('returns null for incorrect month above range (2021-13-10)', () => {
     expect(formatDate('2021-13-10')).toBe(null);
   });
 
-  it('returns date object for 2021-00-10', () => {
+  it('returns null for incorrect month below range (2021-00-10)', () => {
     expect(formatDate('2021-00-10')).toBe(null);
   });
 
-  it('returns date object for 2021-10-32', () => {
+  it('returns null for incorrect day above range (2021-10-32)', () => {
     expect(formatDate('2021-10-32')).toBe(null);
   });
 
-  it('returns date object for 2021-10-00', () => {
+  it('returns null for incorrect day below range (2021-10-00)', () => {
     expect(formatDate('2021-10-00')).toBe(null);
   });
 
-  it('returns date object for 1890-10-05', () => {
-    expect(formatDate('1890-10-05')).toBe(null);
+  it('returns null for incorrect year below range (1899-12-31)', () => {
+    expect(formatDate('1899-12-31')).toBe(null);
   });
 
-  it('returns date object for 2110-10-05', () => {
-    expect(formatDate('2110-10-05')).toBe(null);
+  it('returns null for incorrect year above range (2101-01-01)', () => {
+    expect(formatDate('2101-01-01')).toBe(null);
   });
 });
